2f75a702f652f5f90f0128c9a63acbddea1cc998
alibahieu <trinhtrunghieu2105@gmail.com>
2022-10-05 17:52:11 +0700

    Sidebar 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,13 @@ import Following from '~/pages/Following/index';
 import Profile from '~/pages/Profile/index';
 import Upload from '~/pages/Upload/index';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 //public routes
 const publicRoutes = [
     {path: config.routes.home, component: Home},
     {path: config.routes.following, component: Following},
+    {path: config.routes.live, component: Live},
     {path: config.routes.profile, component: Profile},
     {path: config.routes.upload, component: Upload, layout : HeaderOnly},
     {path: config.routes.search, component: Search, layout : null},
