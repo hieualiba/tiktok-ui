1fe80e372396d7a5d0879e758a86b18d6aafad79
alibahieu <trinhtrunghieu2105@gmail.com>
2022-10-10 17:28:44 +0700

    fix bugs home page

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -17,7 +17,7 @@ function DefaultLayout({children}) {
                 <div className={cx('content')}>{children}</div>
             </div>
             <div className={cx('bottom-btn')}>
-                <Button >Tải ứng dụng</Button>
+                <Button rounded>Tải ứng dụng</Button>
                 <button className={cx('scroll-to-top-btn')}>
                     <svg width="16" height="16" viewBox="0 0 48 48" fill="#FFF" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M22.1086 20.3412C23.1028 19.2196 24.8972 19.2196 25.8914 20.3412L42.8955 39.5236C44.2806 41.0861 43.1324 43.5 41.004 43.5L6.99596 43.5C4.86764 43.5 3.71945 41.0861 5.10454 39.5235L22.1086 20.3412Z"></path>
